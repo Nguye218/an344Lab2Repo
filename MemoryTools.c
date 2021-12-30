dedca5e9fb1c82501ec93e9991dcdfe4b0930267
Andy Nguyen <78712772+Nguye218@users.noreply.github.com>
2021-12-30 10:51:45 -0800

    Create MemoryTools.c

---
+++ MemoryTools.c
@@ -0,0 +1,31 @@
+/*******************************************************************************
+* EECE344 Lab 2
+* MemChkSum() - Fills a block of memory with fillsum
+*
+*
+* Andy Nguyen, 10/18/2021
+* Todd Morton, 9/21/2020 (instructor) 
+*******************************************************************************/
+
+#include "MCUType.h"               /* Include header files                    */
+#include "MemoryTools.h"
+
+
+INT16U MemChkSum(INT8U *startaddr, INT8U *endaddr){
+
+	INT8U *byteptr;
+	INT16U fillsum;
+
+
+	byteptr = endaddr;
+
+	while (byteptr <= startaddr){
+		fillsum = (fillsum + *byteptr);   //adds the values for checksum file
+
+		byteptr++;
+
+
+	}
+	return fillsum;
+
+}
